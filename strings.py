8a35ccc421a260131488bf6ee289ebbd3f9f9cf4
Sainarendra13 <sainarendra06@gmail.com>
2026-06-19 12:29:31 +0530

    Add string manipulation examples in strings.py
    Add various string manipulation examples as comments.

---
+++ strings.py
@@ -0,0 +1,46 @@
+# 1.replace with @
+# str=input()
+# print(str.replace("a","@"))
+
+# 2.print first and last character
+# n=input()
+# print(n[0],n[1] )
+
+# 3. convert into upper and lower case
+# n=input()
+# print(n.upper())
+# print(n.lower())
+
+# 4.remove all spaces
+# space=input()
+# print(space.replace(" ",""))
+
+# 5. print word is in given str
+# word=input()
+# print( "python"in word)
+
+
+# 6.print first three characters of a given string
+# str=input()
+# print(str[0:3])
+
+
+# 7. merge two strings
+# n=input()
+# o=input()
+# print(n+o)
+
+
+# 8. check if string contains only numbers
+# string=input()
+# print(string.isdigit())
+
+
+# 9.remove all occurances of e
+# str=input()
+# print(str.replace("e",""))
+
+
+# 10. count occurances of o
+# str=input()
+# print(str.count("o"))
