bb027f644560ce35375883b2e61834e92db38c97
Sainarendra13 <sainarendra06@gmail.com>
2026-06-21 20:50:21 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,40 @@
+# def nums(x):
+#     if x==0:
+#         return 
+#     else:
+#         print(x)
+#         nums(x-1)
+# nums(10)
+
+
+# def asc(x=1):
+#     if x>n:
+#         return
+#     else:
+#         print(x)
+#         asc(x+1)
+# n=int(input())
+# asc()
+
+
+
+
+# factorial 
+# def fact(n):
+#     if n==0:
+#         return 1
+#     else:
+#        return n*(fact(n-1))
+# n=int(input())
+# print(fact(n))
+
+
+# sum of n numbers
+# def add(n):
+#     if n==0:
+#         return 0
+#     else:
+#         return n+add(n-1)
+# n=int(input())
+# res=add(n)
+# print(res)
